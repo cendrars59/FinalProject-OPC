cbfae6a7918905ac06b118891676bc3971ce4e38
cendrars59 <cyr.leroux.dev@moinslequart.net>
2021-06-25 16:29:19 +0200

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -5,7 +5,7 @@ from practice.models import Practice, Skill
 from users.models import CustomUser
 
 
-@pytest.fixture(params=["chrome1920"])
+@pytest.fixture()
 def driver_init(request):
     """Fixture to use in the context of testing with Selenium. 
     Here the purpose is to test with Chrome on diffrent screen size
@@ -13,18 +13,12 @@ def driver_init(request):
     Args:
         request ([type]): [description]
     """
-    if request.param == "chrome1920":
-        options = webdriver.ChromeOptions()
-        options.add_argument("--headless")
-        options.add_argument("--window-size=1920,1080")
-        web_driver = webdriver.Chrome(options=options)
-        request.cls.browser = "Chrome1920x1080"
-    # if request.param == "chrome411":
-    #     options = webdriver.ChromeOptions()
-    #     options.add_argument("--headless")
-    #     options.add_argument("--window-size=411,823")
-    #     web_driver = webdriver.Chrome(options=options)
-    #     request.cls.browser = "Chrome411x823"
+    
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless")
+    options.add_argument("--window-size=1920,1080")
+    web_driver = webdriver.Chrome(options=options)
+    request.cls.browser = "Chrome1920x1080"
     request.cls.driver = web_driver
     yield
     web_driver.quit()
