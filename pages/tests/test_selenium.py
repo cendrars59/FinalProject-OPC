f3a625951d32b3a723ee5494e1ca3591dd8f6d2a
cendrars59 <cyr.leroux.dev@moinslequart.net>
2021-09-16 21:26:34 +0200

    Fixing Legal selenium test

--- pages/tests/test_selenium.py
+++ pages/tests/test_selenium.py
@@ -11,14 +11,15 @@ class TestPageLegal:
     @pytest.mark.usefixtures("driver_init")
     def test_legal_page(self, live_server, driver_init, user1, user1_involved_in_season1_as_manager):
 
-        self.driver.get(("%s%s" % (live_server.url, "/login")))
+        self.driver.get(("%s%s" % (live_server.url, "/legal")))
         time.sleep(2)
         # Verify the redirection to the login page if user is not connected
+        assert self.driver.current_url == live_server.url + "/login/?next=/legal"
         assert "Login" in self.driver.title
         self.driver.find_element_by_name("username").send_keys(user1.email)
         self.driver.find_element_by_name("password").send_keys("totor")
         self.driver.find_element_by_tag_name("button").click()
-        time.sleep(5)
+        time.sleep(2)
         assert self.driver.current_url == live_server.url + "/legal"
         assert self.driver.find_element_by_id("legal-p1").text != ""
         assert self.driver.find_element_by_id("legal-p2").text != ""
